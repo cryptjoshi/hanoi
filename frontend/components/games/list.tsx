67555de0ecccc0d1981012074d71a7916c63c8a7
cryptjoshi <cryptjoshi@gmail.com>
2024-10-22 14:42:17 +0700

    update

--- frontend/components/games/list.tsx
+++ frontend/components/games/list.tsx
@@ -356,7 +356,7 @@ export default function GameListDataTable({
                 ))}
               </TableHeader>
               <TableBody>
-                {table.getRowModel().rows?.length ? (
+                {table.getRowModel() && table.getRowModel().rows && table.getRowModel().rows.length > 0 ? (
                   table.getRowModel().rows.map((row) => (
                     <TableRow
                       key={row.id}
